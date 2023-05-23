27fa29da9d23f0b8c073758e8b22404fbc3b3ba7
renmission <missionrenjr@gmail.com>
2023-05-23 22:43:47 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+     <header>Amazona</header>
+     <main>
+
+     </main>
+      <footer>
+    All right reserved.
+      </footer>
+    </>
+  )
+}
+
+export default App
